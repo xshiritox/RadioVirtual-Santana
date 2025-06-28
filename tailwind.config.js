5456950ea7675cbf9435c7b0789ac2e0de81edcb
ShiritoDev <mapi27022005@gmail.com>
2025-06-28 17:06:00 -0400

    Fix Tailwind CSS styles not loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html', 
+    './src/**/*.{js,ts,jsx,tsx,vue}'
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -57,4 +60,4 @@ export default {
     },
   },
   plugins: [],
-};
+}
